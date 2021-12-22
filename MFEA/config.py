a6eb85a5b0005fe6d9c1c08f5bbdc8d83f6c41a4
Phan Thanh Long <driverofl01@gmail.com>
2021-12-23 01:39:42 +0700

    init

---
+++ MFEA/config.py
@@ -0,0 +1,4 @@
+DATA_PATH = ['../data/eil10.tsp', '../data/eil51.tsp']
+POPULATION_SIZE = 6  # phải là số lẻ vì lúc sih luôn sinh theo cặp và cái đầu tiên là None rồi 
+NUM_GENERATIONS = 10
+RMP = 0.5
